30c372ac2c090ac2f2ea58bf25bca18f3a3fa676
repeatMe <113420397+repeatMe@users.noreply.github.com>
2024-01-30 12:16:17 +0530

    Update FormPage.java

--- src/main/java/com/mobileTesting/pageObjects/android/FormPage.java
+++ src/main/java/com/mobileTesting/pageObjects/android/FormPage.java
@@ -44,7 +44,7 @@ public class FormPage extends AndroidActions{
 		
 	
 	public void setNameField(String name) throws InterruptedException {
-		thread.sleep(2000);
+		
 		nameField.sendKeys(name);
         driver.hideKeyboard();	
 	}
